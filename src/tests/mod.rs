d7814add5efa2a00c59dd4dc99289cdf2b017929
Joseph Gimenez <joseph.gimenez@snagajob.com>
2017-06-11 10:45:02 -0400

    extract unit test data to separate file

---
+++ src/tests/mod.rs
@@ -0,0 +1,30 @@
+extern crate serde_json;
+
+use notificationcenter;
+use std::io::BufReader;
+use std::fs::File;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn app_id_missing() {
+        let expected_error = "app_id not found for application name: app_name";
+        let file = File::open("src/tests/app_id_missing.json").expect("Json file not found.");
+        let file_reader = BufReader::new(file);
+
+        let config_json = serde_json::from_reader(file_reader)
+            .expect("Couldn't parse json file.  Validate with json linter to confirm.");
+
+        let conn = notificationcenter::open_notificationcenter_db();
+        let notes = notificationcenter::populate_app_notes(&config_json, &conn);
+
+        match notes {
+            Ok(val) => panic!("Received: {:?} but expected error: {}", val, expected_error),
+            Err(err) => {
+                assert_eq!(err, expected_error);
+            }
+        }
+    }
+}
